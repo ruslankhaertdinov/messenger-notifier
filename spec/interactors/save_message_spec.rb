f175b3b36361afb72a557353cf4271c36b71593c
Ruslan Khaertdinov <ruslanimos@ya.ru>
2019-04-28 14:44:19 +0300

    Обновил тесты

--- spec/interactors/save_message_spec.rb
+++ spec/interactors/save_message_spec.rb
@@ -4,18 +4,31 @@ describe SaveMessage do
   let(:interactor) { described_class.call(params: params) }
   let(:params) do
     {
-      whats_app: %w[90611122233 90611122244],
-      viber: %w[90611122255 90611122266],
-      telegram: %w[username1 username2],
-      send_at: '2019-04-28T14:21:58+03:00',
+      whats_app: %w[90611122233],
+      viber: %w[90611122255 ],
+      telegram: %w[@username1],
+      send_at: send_at,
       message: 'Привет'
     }.stringify_keys
   end
 
-  context 'данные валидны' do
+  context 'время отправки передано' do
+    let(:send_at) { '2019-04-28T14:21:58+03:00' }
+
     it 'сохранит все данные' do
-      expect { interactor }.to change { UserMessage.count }.by(6)
+      expect { interactor }.to change { UserMessage.count }.by(3)
       expect(interactor).to be_success
+      expect(UserMessage.pluck(:send_at).size).to eq(3)
+    end
+  end
+
+  context 'время отправки не передано' do
+    let(:send_at) { nil }
+
+    it 'сохранит время отправки добавит автоматически' do
+      expect { interactor }.to change { UserMessage.count }.by(3)
+      expect(interactor).to be_success
+      expect(UserMessage.pluck(:send_at).size).to eq(3)
     end
   end
 end
